5c298d57a16463c4458faddc7d97c951fd15561e
Priyanshu Pandey <priyanshun14314@gmail.com>
2026-08-26 01:15:22 +0530

    Added Analysis model

--- src/main/java/com/resumematcher/resume_matcher/models/Analysis.java
+++ src/main/java/com/resumematcher/resume_matcher/models/Analysis.java
@@ -1,4 +1,34 @@
 package com.resumematcher.resume_matcher.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="analysis")
 public class Analysis {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    private String restext;
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String jobDescription;
+    private int matchScore;
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String mskills; //missing skills in resume
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String suggestions;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
